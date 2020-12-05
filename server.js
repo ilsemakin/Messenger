a6e70419b1f23aea3a5bf25d0c4a0926c2d4fc8c
Ilya Semakin <il.semakin@mail.ru>
2020-12-05 17:56:38 +0300

    Library connection for web applications and real-time data exchange

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express = require('express');
 
 const app = express(); // Server application
 
+/* To process queries logic */
+const server = require('http').createServer(app); 
+const io = require('socket.io')(server);
+
+
+
 app.get('/user', (req, res) => {
   console.log("Hello");
   res.json();
 });
 
-app.listen(3000, (error) => {
+server.listen(3000, (error) => {
   if (error) { throw Error(error); }
   console.log("Server started!");
 });
