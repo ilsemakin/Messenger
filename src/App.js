85f40a2bec628b711405be1677a1ffc5ea8234e2
Ilya Semakin <il.semakin@mail.ru>
2020-12-06 18:57:34 +0300

    Working with hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+
 import JoinBlock from './components/Registration';
+
 import socket from './socket';
+import axios from 'axios';
+import reducer from './reducer';
 
 function App() {
-  return(<div className="wrapper"><JoinBlock /></div>);
+  const [state, dispatch] = React.useReducer(reducer, {
+    joined: false, roomId: null, userName: null, users: [], messages: [],
+  });
+
+  const onLogin = () => {
+    dispatch({type: 'JOINED', payload: true});
+  };
+
+  return(<div className="wrapper"><JoinBlock onLogin={onLogin} /></div>);
 }
 
 export default App;
