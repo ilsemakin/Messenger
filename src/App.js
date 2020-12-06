c06791ddcab12c2cd071ea3710a79b0e8d5f209f
Ilya Semakin <il.semakin@mail.ru>
2020-12-06 18:03:21 +0300

    Work with application pages will be in other files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import JoinBlock from './components/Registration';
+import socket from './socket';
+
 function App() {
-  return <div className="App">Hello</div>;
+  return(<div className="wrapper"><JoinBlock /></div>);
 }
 
 export default App;
