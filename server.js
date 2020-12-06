765715dd9e5ca62b6140d5fedf020d0d8eedbcff
Ilya Semakin <il.semakin@mail.ru>
2020-12-06 22:33:09 +0300

    Connect to a room, chat and disconnect from it

--- server.js
+++ server.js
@@ -14,8 +14,20 @@ app.use(express.json());
 /* Storing rooms */
 const rooms = new Map();
 
-app.get('/home', (req, res) => {
-  res.json(rooms);
+app.get('/home/:id', (req, res) => {
+  const { id: roomId } = req.params;
+  
+  /* 
+  * Checking the existence of a room
+  * If the room exists, return the actual values
+  * Otherwise, empty values are returned (there are no users and messages)
+  */
+  const obj = rooms.has(roomId) ? {
+    users: [...rooms.get(roomId).get('users').values()],
+    messages: [...rooms.get(roomId).get('messages').values()],
+  } : {users: [], messages: []};
+
+  res.json(obj);
 });
 
 app.post('/home', (req, res) => {
@@ -25,6 +37,23 @@ app.post('/home', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  
+  socket.on('ROOM:JOIN', ({roomId, userName}) => {
+    socket.join(roomId); // Join a user to a room
+    rooms.get(roomId).get('users').set(socket.id, userName); // Transferring user data to the room
+    const users = [...rooms.get(roomId).get('users').values()]; // Save all connected users
+    socket.to(roomId).broadcast.emit('ROOM:USER_STATUS', users); // Notifying all users about a new connection
+  });
+
+  socket.on('disconnect', () => {
+    rooms.forEach((value, roomId) => {
+      if (value.get('users').delete(socket.id)) {
+        const users = [...value.get('users').values()]; // Save all connected users
+        socket.to(roomId).broadcast.emit('ROOM:USER_STATUS', users); // Alerting all users when a user is disconnected
+      }
+    });
+  });
+
   console.log('User connected', socket.id);
 });
 
