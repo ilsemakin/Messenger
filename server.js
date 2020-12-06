a0f5a6d113f3cbe7598826394c76b91c81817613
Ilya Semakin <il.semakin@mail.ru>
2020-12-06 17:59:50 +0300

    Test request processing

--- server.js
+++ server.js
@@ -7,14 +7,22 @@ const app = express(); // Server application
 const server = require('http').createServer(app); 
 const io = require('socket.io')(server);
 
+app.use(express.json());
+
 const rooms = new Map(); // Storing rooms
 
 app.get('/home', (req, res) => {
   res.json(rooms);
 });
 
+app.post('/home', (req, res) => {
+  const { rooId, userName } = req.body;
+  if (!rooms.has(roomId)) { rooms.set(rooId, new Map([ ['users', new Map()], ['messages', []] ])); }
+  res.send();
+});
+
 io.on('connection', (socket) => {
-  console.log('user connected', socket.id);
+  console.log('User connected', socket.id);
 });
 
 server.listen(8080, (error) => {
